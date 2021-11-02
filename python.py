2b0f1e5a65489cc05ffce6f59caa51e42842b4e6
manas-rai <82706582+manas-rai@users.noreply.github.com>
2021-11-02 16:02:06 +0530

    Create python.py

---
+++ python.py
@@ -0,0 +1,11 @@
+def greater(a,b):
+  if a>b:
+    return 'a is greater'
+  elif b>a:
+    return 'b is greater'
+  else:
+    return 'both are equal'
+x=int(input())
+y=int(input())
+c=greater(x,y)
+print(c)
